4e43918c6d1c3a9a87f34f71438d0ad84c8a2dce
t0702 <t0702@naver.com>
2020-03-14 08:47:36 +0900

    20/03/14

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(document).mousemove(function(e){
+        var mouseX = e.pageX;
+        var mouseY = e.pageY;
+        $(".cursor").css({
+            left: mouseX+"px",
+            top:mouseY+"px"
+        });
+    });
+});
