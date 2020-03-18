5c272f01c70dbe8afb6e050889d498ad7aa9be59
t0702 <t0702@naver.com>
2020-03-18 20:49:57 +0900

    2020/03/18

--- accordion/script.js
+++ accordion/script.js
@@ -5,10 +5,21 @@ var btnCollapse = document.getElementById('btn-collapse'),
 
 //제목을 클릭하면 할 일
 for(var i = 0; i < heading.length; i++){ //헤딩들 마다 할 일
-    heading[i].addEventListener('click', function(){
+    heading[i].addEventListener('click', function(e){
         for( var j = 0; j < question.length; j++){
             question[j].classList.remove('active');
+            e.target.parentNode.classList.add('active'); //parentNode > 부모
+            activateBody();
         }//question들 마다 할 일
     });
 }
 
+function activateBody(){
+    // 1. panel-body 모두 안보이도록 answer / css의 경우 display:none;
+    for(var x = 0; x < answer.length; x++){
+        answer[x].style.display = 'none';
+    }
+
+    // 2. 클래스명 active가 있는 panel-question 자식 중 panel-body 나타나도록
+    
+}
