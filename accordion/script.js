acdb8c58efc18e2cddefc88d59fb2600c9cfc997
t0702 <t0702@naver.com>
2020-03-18 19:47:33 +0900

    2020/03/18

---
+++ accordion/script.js
@@ -0,0 +1,8 @@
+var btnCollapse = document.getElementById('btn-collapse'),
+    heading = document.getElementsByClassName('panel-heading'),
+    question = document.getElementsByClassName('panel-question'),
+    answer = document.getElementsByClassName('panel-body');
+
+//제목을 클릭하면 할 일
+heading
+
