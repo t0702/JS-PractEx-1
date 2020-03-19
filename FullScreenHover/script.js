e4b84a028767036707208d5c7e07a54ea066fa04
t0702 <t0702@naver.com>
2020-03-19 20:16:41 +0900

    2020/03/19

---
+++ FullScreenHover/script.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('.container a');
+const bg = document.querySelector('.bg');
+const showClass = 'bg-show';
+//아래서 link는 맘대로 지정하는 것.
+for(const link of links){
+    
+    link.addEventListener('mouseenter',function(){
+        bg.style.backgroundImage = `url(${this.dataset.bg})`;
+        document.body.classList.add(showClass);
+    });
+    link.addEventListener('mouseleave',function(){
+        document.body.classList.remove(showClass);
+    });
+}//for 반복문
