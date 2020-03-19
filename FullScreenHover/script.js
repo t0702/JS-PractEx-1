86c5059a62b5020c59684d8d04d636dc3aabde2f
t0702 <t0702@naver.com>
2020-03-19 20:17:59 +0900

    2020/03/19

--- FullScreenHover/script.js
+++ FullScreenHover/script.js
@@ -3,7 +3,10 @@ const bg = document.querySelector('.bg');
 const showClass = 'bg-show';
 //아래서 link는 맘대로 지정하는 것.
 for(const link of links){
-    
+    const img = new Image();
+    img.src = link.dataset.bg;
+    console.log(img);
+
     link.addEventListener('mouseenter',function(){
         bg.style.backgroundImage = `url(${this.dataset.bg})`;
         document.body.classList.add(showClass);
